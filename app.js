f36b64a8695af4dfb0783fc1c74b811c97ed9f3c
Vladimir Milenko <vladimir.milenko@uber.com>
2020-02-28 15:58:53 +0000

    Add slack render

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ import * as components from './slack-renderer/components';
 import * as container from './slack-renderer/container';
 import SlackDom from './slack-renderer/index';
 
-export default () => ({
-  components,
-  container,
-  SlackDom
-})
+window.SlackRender = function SlackRender() {
+  return {
+    components,
+    container,
+    SlackDom
+  }
+}
