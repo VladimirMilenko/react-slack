bb619d8d3ed1e4d25072778bd431fb04755db65a
Vladimir Milenko <vladimir.milenko@uber.com>
2020-02-28 14:55:17 +0000

    Default fn

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { SlackContainer } from "./slack-renderer/container";
+import * as components from './slack-renderer';
+import * as container from './slack-renderer/container';
+import SlackDom from './slack-renderer';
 
-
-export * from './slack-renderer/components';
-export * from './slack-renderer/container';
-export SlackDom from './slack-renderer';
+export default () => ({
+  components,
+  container,
+  SlackDom
+});
