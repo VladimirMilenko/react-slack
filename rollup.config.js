349ec64dc7668a330fbebd3d780709422897012e
Vladimir Milenko <vladimir.milenko@uber.com>
2020-02-28 16:19:35 +0000

    Try parcel build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import path from 'path';
 import fs from 'fs';
 import babel from 'rollup-plugin-babel';
+import nodeGlobals from 'rollup-plugin-node-globals';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 let pkg = JSON.parse(fs.readFileSync('./package.json')),
   external = Object.keys(pkg.dependencies || {}),
@@ -26,6 +28,7 @@ export default {
       jsnext: true,
       main: true,
     }),
+    nodeGlobals({ process: true, global: true }),
     commonjs({
       include: 'node_modules/**',
     })
